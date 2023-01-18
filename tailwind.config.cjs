cd9ba3f0458320866c44ebd568bb6eebb59b0469
howbizarre <howbizarre@gmail.com>
2023-01-18 17:50:46 +0200

    ADD: Dark theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./index.html",
     "./lib/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        'sm': '2rem',
+        'lg': '4rem',
+        'xl': '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {},
   },
   plugins: [],

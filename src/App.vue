7d314d8d68cd3ee9bb7c3a204e29040f7ae96d01
howbizarre <howbizarre@gmail.com>
2023-01-18 15:51:39 +0200

    ADD: Pass parameter to the caller

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
 import VueMonthCalendar from "../lib/VueMonthCalendar.vue";
+
+function getDate(activeDate: { month: number, year: number, date: number }) {
+  console.group("The returned data is:");
+  console.log("Day: ", activeDate.date);
+  console.log("Month: ", activeDate.month);
+  console.log("Yeaar: ", activeDate.year);
+  console.groupEnd();
+}
 </script>
 
 <template>
-  <VueMonthCalendar msg="Vue Month Calendar" />
+  <VueMonthCalendar @get-date="getDate" />
 </template>
 

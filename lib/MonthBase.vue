255559a9d4557510f59c0d0f83f1918e585d96a2
howbizarre <howbizarre@gmail.com>
2023-01-25 10:00:22 +0200

    UPD: Max width under 640px

--- lib/MonthBase.vue
+++ lib/MonthBase.vue
@@ -18,7 +18,7 @@
     bg-white dark:bg-slate-800
     ring-1 ring-zinc-900/5 dark:ring-zinc-600/25
     shadow-xl dark:shadow-zinc-800
-    rounded-lg p-3 w-[320px] sm:w-[350px] relative;
+    rounded-lg p-3 max-[300px]:w-[300px] max-[480px]:w-full sm:w-[350px] relative;
 }
 
 .month-grid { @apply grid grid-cols-7 gap-y-1 sm:gap-y-2 py-1 sm:py-3; }
